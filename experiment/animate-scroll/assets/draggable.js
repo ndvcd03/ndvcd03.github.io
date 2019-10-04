dd4bcc074a9bb5f3af66b5146a3f48aa4aaaab65
jkauffm1 <kauffman.jordan@gmail.com>
2019-10-04 16:54:45 -0400

    Added some Experiments

---
+++ experiment/animate-scroll/assets/draggable.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+$( function() {
+    $("#draggable").draggable();
+    $( "#love" ).draggable();
+    $( "#zahar" ).draggable();
+  });
+  });
---
+++ experiment/animate-scroll/assets/rocket-launch.jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#rocket-button').click(function() {
+    $("#rocket").toggleClass("launch");
+  });
+
+  $('#rocket-button2').click(function() {
+    $("#rocket-2").toggleClass("launch-2");
+  });
+});
